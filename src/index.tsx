e206be4595b6644107c4d2f0bd589dbd38c4aa8c
james miked dupont <jmikedupont2@gmail.com>
2021-07-27 20:55:09 -0400

    ported the slider over to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,203 @@
 import * as THREE from 'three';
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { RangeStepInput } from 'react-range-step-input';
+//import { RangeStepInput } from 'oreact-range-step-input';
 
-let points: THREE.Points;
-let scene: THREE.Scene;
-let camera: THREE.PerspectiveCamera;
-let renderer: THREE.WebGLRenderer;
+//import PropTypes from 'prop-types';
 
-const forceNumber = function(n: any) {
+/* RangeStepInput.propTypes = {
+ *     value: PropTypes.number.isRequired,
+ *     onChange: PropTypes.func.isRequired,
+ *     step: PropTypes.number.isRequired,
+ *     className: PropTypes.string,
+ *     min: PropTypes.number,
+ *     max: PropTypes.number,
+ *     id: PropTypes.string,
+ *     name: PropTypes.string,
+ *     disabled: PropTypes.bool,
+ *     style: PropTypes.string,
+ * 
+ *     // Determines whether the slider changes value when the cursor is
+ *     // held on it.
+ *     hold: PropTypes.bool
+ * };
+ *  */
+interface MyProps1 {
+    max: number;
+    min: number;
+    step: number;
+    value: number;
+    name?: string;
+    id?: string;
+    className?: string;
+    style?: React.CSSProperties;
+    disabled?: boolean;
+    onChange: any;
+    onClick?: any;
+    hold?: boolean;
+}
+
+interface MyState {
+    isMouseDown: boolean;
+    isDragging: boolean;
+}
+
+// lifted from https://reactjsexample.com/a-range-step-input-for-react/
+class RangeStepInput extends React.Component<MyProps1, MyState> {
+    private domRef: any;
+    private holdLoop: any;
+    private onClick: any;
+
+    constructor(props) {
+
+        super(props);
+        this.state = {
+            isMouseDown: false,
+            isDragging: false
+        };
+        this.onInput = this.onInput.bind(this);
+        this.onMouseDown = this.onMouseDown.bind(this);
+        this.onMouseUp = this.onMouseUp.bind(this);
+        this.onMouseMove = this.onMouseMove.bind(this);
+
+        this.domRef = React.createRef();
+    }
+    render() {
+        return <input
+            type="range"
+            ref={this.domRef}
+            //            className={this.props.className}
+            min={this.props.min}
+            max={this.props.max}
+            step={this.props.step}
+            value={this.props.value}
+            //            name={this.props.name}
+            //            id={this.props.id}
+            //            style={this.props.style}
+            disabled={this.props.disabled}
+            onChange={this.props.onChange}
+            onMouseDown={this.onMouseDown}
+            onMouseUp={this.onMouseUp}
+            onMouseMove={this.onMouseMove}
+            onClick={this.onClick}
+            onInput={this.onInput} />;
+    }
+    onMouseDown() {
+        this.setState({ isMouseDown: true });
+
+        if (this.props.hold) {
+            if (this.holdLoop) {
+                clearInterval(this.holdLoop);
+            }
+
+            let oldVal = this.props.value;
+
+            const self = this;
+            setTimeout(function() {
+                if (self.holdLoop) {
+                    clearInterval(self.holdLoop);
+                }
+                self.holdLoop = self.makeHoldLoop(oldVal);
+                // Add some initial delay on the click-hold functionality.
+            }, 250);
+        }
+    }
+    onMouseUp() {
+        this.setState({
+            isMouseDown: false,
+            isDragging: false
+        });
+
+        if (this.holdLoop) {
+            clearInterval(this.holdLoop);
+        }
+    }
+    onMouseMove() {
+        if (this.state.isMouseDown) {
+            this.setState({ isDragging: true });
+        }
+    }
+    onInput(e) {
+        const step = this.props.step;
+        const newVal = forceNumber(e.target.value);
+        const oldVal = this.props.value;
+
+        if (
+            // Disable the oninput filter with the user is dragging
+            // the slider's knob.
+            !(this.state.isMouseDown && this.state.isDragging) &&
+            oldVal
+        ) {
+            e.target.value = (newVal > oldVal) ?
+                oldVal + step : oldVal - step;
+        }
+    }
+    makeHoldLoop(oldVal) {
+        const self = this;
+
+        return setInterval(function() {
+            if (!self.state.isMouseDown || self.state.isDragging) {
+                // The user isn't holding the cursor anymore, or the cursor
+                // is being dragged. Clean up and cancel.
+                if (self.holdLoop) {
+                    clearInterval(self.holdLoop);
+                }
+                return false;
+            }
+
+            const input = self.domRef.current;
+            let newVal = self.props.value;
+
+            if (
+                oldVal > newVal &&
+                (newVal - self.props.step) >= self.props.min
+            ) {
+                newVal -= self.props.step;
+            } else if (
+                oldVal < newVal &&
+                (newVal + self.props.step) <= self.props.max
+            ) {
+                newVal += self.props.step;
+            }
+
+            if (oldVal === newVal) {
+                return false;
+            }
+
+            // Directly setting input.value will cause the new value
+            // to not be recognized, because of React.
+            // https://stackoverflow.com/a/46012210/173630
+            const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+                window.HTMLInputElement.prototype, 'value').set;
+            nativeInputValueSetter.call(input, newVal);
+
+            // Trigger an onChange event.
+            const e = new Event('change', { bubbles: true });
+
+            return input.dispatchEvent(e);
+        }, 100);
+    }
+};
+
+
+/* RangeStepInput.defaultProps = {
+ *     hold: true
+ * };
+ *  */
+
+const forceNumber = function(n) {
     n = Number(n);
     if (isNaN(n) || typeof n === 'undefined') {
         n = 0;
     }
     return n;
 };
+
+let points: THREE.Points;
+let scene: THREE.Scene;
+let camera: THREE.PerspectiveCamera;
+let renderer: THREE.WebGLRenderer;
+
 
 interface MyProps {
     fov: number;
@@ -57,7 +240,7 @@ class App extends React.Component<IProps, MyProps> {
                 </span>
             </div>
             <div>
-                Model Size:
+                Model Size: (x,y,z)
                 <span>
                     <RangeStepInput
                         min={1}
@@ -66,7 +249,7 @@ class App extends React.Component<IProps, MyProps> {
                         value={this.state.xsize}
                         onChange={this.onChangeXSize.bind(this)}
                     />
-                    <span className="input-group-text ml-2">
+                    X<span className="input-group-text ml-2">
                         {this.state.xsize}
                     </span>
                     <RangeStepInput
@@ -76,7 +259,7 @@ class App extends React.Component<IProps, MyProps> {
                         value={this.state.ysize}
                         onChange={this.onChangeYSize.bind(this)}
                     />
-                    <span className="input-group-text ml-2">
+                    Y<span className="input-group-text ml-2">
                         {this.state.ysize}
                     </span>
 
@@ -87,7 +270,7 @@ class App extends React.Component<IProps, MyProps> {
                         value={this.state.zsize}
                         onChange={this.onChangeZSize.bind(this)}
                     />
-                    <span className="input-group-text ml-2">
+                    Z<span className="input-group-text ml-2">
                         {this.state.zsize}
                     </span>
 
@@ -95,7 +278,7 @@ class App extends React.Component<IProps, MyProps> {
             </div>
 
             <div>
-                Model Rotation:
+                Model Rotation: (x,y,z)
                 <span>
                     <RangeStepInput
                         min={-1}
@@ -104,7 +287,7 @@ class App extends React.Component<IProps, MyProps> {
                         value={this.state.xrot}
                         onChange={this.onChangeXRot.bind(this)}
                     />
-                    <span className="input-group-text ml-2">
+                    X <span className="input-group-text ml-2">
                         {this.state.xrot}
                     </span>
                     <RangeStepInput
@@ -114,7 +297,7 @@ class App extends React.Component<IProps, MyProps> {
                         value={this.state.yrot}
                         onChange={this.onChangeYRot.bind(this)}
                     />
-                    <span className="input-group-text ml-2">
+                    Y <span className="input-group-text ml-2">
                         {this.state.yrot}
                     </span>
 
@@ -125,7 +308,7 @@ class App extends React.Component<IProps, MyProps> {
                         value={this.state.zrot}
                         onChange={this.onChangeZRot.bind(this)}
                     />
-                    <span className="input-group-text ml-2">
+                    Z <span className="input-group-text ml-2">
                         {this.state.zrot}
                     </span>
 
@@ -172,7 +355,9 @@ class App extends React.Component<IProps, MyProps> {
         scene.remove(points);
         points = createPoints(this.state.xsize, this.state.ysize, this.state.zsize, this.state.scale, this.state.nodeSize);
         scene.add(points);
-        renderer.render(scene, camera);
+        if (renderer) {
+            renderer.render(scene, camera);
+        }
         gstate = this.state;
     }
     onChangeXSize(e: any) {
@@ -269,7 +454,9 @@ function createPoints(xsize: number, ysize: number, zsize: number, scale: number
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
+    if (renderer) {
+        renderer.setSize(window.innerWidth, window.innerHeight)
+    }
     render()
 }
 
@@ -284,18 +471,20 @@ function animate() {
 }
 
 function render() {
-    renderer.render(scene, camera)
+    if (renderer) {
+        renderer.render(scene, camera)
+    }
 }
 
 
 export default function main() {
 
-    const scene = new THREE.Scene()
+    scene = new THREE.Scene()
 
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
     camera.position.z = 2
 
-    const renderer = new THREE.WebGLRenderer()
+    renderer = new THREE.WebGLRenderer()
     renderer.setSize(window.innerWidth, window.innerHeight)
     document.body.appendChild(renderer.domElement)
 
@@ -308,7 +497,7 @@ export default function main() {
 
         xrot: 0,
         yrot: 0,
-        zrot: 0.0001,
+        zrot: 0.001,
 
         scale: 0.4,
         nodeSize: 0.1,
